d08fb85a65f921f509e05650cca466c0f44b7c6e
rsupapo-UTD <144633920+rsupapo-UTD@users.noreply.github.com>
2024-10-27 16:59:16 -0500

    script now contains all relations from schema dictionary
    no bugs, all tables successfully created.

--- script.sql
+++ script.sql
@@ -1,4 +1,3 @@
-
 CREATE TABLE Account (
     AccountID INT,
     Address VARCHAR(80),
@@ -7,54 +6,55 @@ CREATE TABLE Account (
     PRIMARY KEY (AccountID)
 );
 
-CREATE TABLE Payment_Method (
+CREATE TABLE PaymentMethod (
     Card_Number INT,
     Cardholder_Name VARCHAR(50),
     Email VARCHAR(50),
-    FK_AccountID int,
-    PaymentID int,
+    FK_AccountID INT,
+    PaymentID INT,
     PRIMARY KEY (PaymentID),
-    foreign key (FK_AccountID) references Account (AccountID)
+    FOREIGN KEY (FK_AccountID)
+        REFERENCES Account (AccountID)
 );
     
-    CREATE TABLE ShoppingCart (
-    AccountID int,
-    AmountOfItems int,
-    OrderID int,
-    TotalAmount decimal,
+CREATE TABLE ShoppingCart (
+    AccountID INT,
+    AmountOfItems INT,
+    OrderID INT,
+    TotalAmount DECIMAL,
     PRIMARY KEY (OrderID)
 );
     
-   CREATE TABLE Supplier (
-    ContactEmail varchar(30),
-    SupplierID int,
-    SupplierName varchar(30),
+CREATE TABLE Supplier (
+    ContactEmail VARCHAR(30),
+    SupplierID INT,
+    SupplierName VARCHAR(30),
     PRIMARY KEY (SupplierID)
-); 
+);
 
-    
 CREATE TABLE Product (
-	`Condition` VARCHAR(30),
-    Description Varchar(200),
+    `Condition` VARCHAR(30), -- KEYWORD
+    `Description` VARCHAR(200), -- KEYWORD
     FK_OrderID INT,
-    FK_SupplierID int,
-    ProductID int,
-    ProductName VarChar(80),
-    Price decimal,
-    Quantity int,
+    FK_SupplierID INT,
+    ProductID INT,
+    ProductName VARCHAR(80),
+    Price DECIMAL,
+    Quantity INT,
     PRIMARY KEY (ProductID),
-    FOREIGN KEY (FK_OrderID) REFERENCES ShoppingCart (OrderID),
-    FOREIGN KEY (FK_SupplierID) REFERENCES Supplier (SupplierID)
+    FOREIGN KEY (FK_OrderID)
+        REFERENCES ShoppingCart (OrderID),
+    FOREIGN KEY (FK_SupplierID)
+        REFERENCES Supplier (SupplierID)
 );
     
 CREATE TABLE Category (
     CategoryID INT,
-    CategoryName varchar(80),
-    CategoryDescription varchar(200),
+    CategoryName VARCHAR(80),
+    CategoryDescription VARCHAR(200),
     PRIMARY KEY (CategoryID)
 );
 
-    
 CREATE TABLE ACCOUNT_ADDRESS (
     AccountAddress VARCHAR(80),
     FK_Account_ID INT,
@@ -63,83 +63,119 @@ CREATE TABLE ACCOUNT_ADDRESS (
         REFERENCES Account (AccountID)
 );
 
-
 CREATE TABLE CATEGORIZES (
-    FK_CategoryID int,
-    FK_ProductID int,
-    PRIMARY KEY (FK_CategoryID, FK_ProductID),
-    foreign key (FK_CategoryID) references Category(CategoryID),
-    foreign key (FK_ProductID) references Product(ProductID)
+    FK_CategoryID INT,
+    FK_ProductID INT,
+    PRIMARY KEY (FK_CategoryID , FK_ProductID),
+    FOREIGN KEY (FK_CategoryID)
+        REFERENCES Category (CategoryID),
+    FOREIGN KEY (FK_ProductID)
+        REFERENCES Product (ProductID)
 );
 
 CREATE TABLE Customer (
-    AccountID int,
-    NumProductsBought int,
+    AccountID INT,
+    NumProductsBought INT,
     PRIMARY KEY (AccountID),
-    FOREIGN KEY (AccountID) references Account(AccountID)
+    FOREIGN KEY (AccountID)
+        REFERENCES Account (AccountID)
 );
 
 CREATE TABLE Customer_Business (
-    AccountID int,
-    BusinessName varchar(50),
-    ContactPerson varchar(30),
-    TaxID int,
-    primary key (AccountID),
-    foreign key (AccountID) references Account(AccountID)
+    AccountID INT,
+    BusinessName VARCHAR(50),
+    ContactPerson VARCHAR(30),
+    TaxID INT,
+    PRIMARY KEY (AccountID),
+    FOREIGN KEY (AccountID)
+        REFERENCES Account (AccountID)
 );
 
 CREATE TABLE Customer_Person (
-    AccountID int,
-    DateOfBirth VarChar(10),
-    PRIMARY KEY (AccountID), 
-    FOREIGN KEY (FK_Account_ID) REFERENCES Account (AccountID)
+    AccountID INT,
+    DateOfBirth VARCHAR(10),
+    PRIMARY KEY (AccountID),
+    FOREIGN KEY (AccountID)
+        REFERENCES Account (AccountID)
 );
 
 CREATE TABLE Debit_Credit (
-    CardNumber int,
+    CardNumber INT,
     CardholderName VARCHAR(30),
-    CVV int,
-    ExpirationDate VarChar(4),
-    FK_Payment_ID int,
+    CVV INT,
+    ExpirationDate VARCHAR(4),
+    FK_Payment_ID INT,
     PRIMARY KEY (FK_Payment_ID),
-    foreign key (FK_Payment_ID) references PaymentMethod (Payment_ID)
+    FOREIGN KEY (FK_Payment_ID)
+        REFERENCES PaymentMethod (PaymentID)
 );
 
 CREATE TABLE Discounts (
-    DiscountCode varchar(12),
-    FK_ProductID int,
+    DiscountCode VARCHAR(12),
+    FK_ProductID INT,
     PRIMARY KEY (FK_ProductID),
-    foreign key (FK_ProductID) references ShoppingCart(OrderID)
+    FOREIGN KEY (FK_ProductID)
+        REFERENCES ShoppingCart (OrderID)
 );
 
-CREATE TABLE `Order` (
-    OrderDate varchar(10),
-    Address VARCHAR(80),
-    Email VARCHAR(30),
-    PhoneNumber VARCHAR(14),
-    PRIMARY KEY (AccountID)
-);
-
-CREATE TABLE Review (
-    FK_ProductID INT,
-    ReviewID int,
-    ReviewStar decimal,
-    PRIMARY KEY (ReviewID),
-    foreign key (FK_ProductID) references Product (ProductID)
+CREATE TABLE Orders (
+    OrderDate VARCHAR(10),
+    FK_OrderID INT,
+    OrderStatus VARCHAR(50),
+    PRIMARY KEY (FK_OrderID),
+    FOREIGN KEY (FK_OrderID)
+        REFERENCES ShoppingCart (OrderID)
 );
 
 CREATE TABLE Review (
     FK_ProductID INT,
-    ReviewID int,
-    ReviewStar decimal,
+    ReviewID INT,
+    ReviewStar DECIMAL,
     PRIMARY KEY (ReviewID),
-    foreign key (FK_ProductID) references Product(ProductID)
+    FOREIGN KEY (FK_ProductID)
+        REFERENCES Product (ProductID)
 );
 
 CREATE TABLE Seller (
-	AccountID int,
-    NumProductsSold int,
-    Primary key (AccountID),
-    Foreign key (AccountID) references Account (AccountID)
-    );
+    AccountID INT,
+    NumProductsSold INT,
+    PRIMARY KEY (AccountID),
+    FOREIGN KEY (AccountID)
+        REFERENCES Account (AccountID)
+);
 
+CREATE TABLE Seller_Business (
+    AccountID INT,
+    BusinessName VARCHAR(50),
+    ContactPerson VARCHAR(30),
+    TaxID INT,
+    PRIMARY KEY (AccountID),
+    FOREIGN KEY (AccountID)
+        REFERENCES Account (AccountID)
+);
+
+CREATE TABLE Seller_Person (
+    AccountID INT,
+    DateOfBirth VARCHAR(10),
+    FirstName VARCHAR(15),
+    LastName VARCHAR(15),
+    PRIMARY KEY (AccountID),
+    FOREIGN KEY (AccountID)
+        REFERENCES Account (AccountID)
+);
+
+CREATE TABLE ThirdParty (
+    FK_PaymentID INT,
+    Password VARCHAR(30),
+    PRIMARY KEY (FK_PaymentID),
+    FOREIGN KEY (FK_PaymentID)
+        REFERENCES PaymentMethod (PaymentID)
+);
+
+CREATE TABLE WireTransfer (
+    AccountNumber INT,
+    FK_PaymentID INT,
+    PRIMARY KEY (FK_PaymentID),
+    FOREIGN KEY (FK_PaymentID)
+        REFERENCES PaymentMethod (PaymentID)
+);
